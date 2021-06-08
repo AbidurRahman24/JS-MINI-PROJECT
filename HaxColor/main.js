9a067dc8266b22df222ce7320d9b221620e32930
Niloy-Sumon <niloyabidur@gmail.com>
2021-06-08 17:31:13 +0600

    hex code

---
+++ HaxColor/main.js
@@ -0,0 +1,18 @@
+const genaratorColor = () =>{
+    let hexCode = ['1' ,'2' ,'3' ,'4' ,'5' ,'6' ,'7' ,'8' ,'9' ,'a' ,'b' ,'c' ,'d' ,'e' ,'f' ]
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        let num = Math.round(Math.random() * 14);
+       
+        let num2 = hexCode[num];
+
+		color += num2;
+    }
+
+
+    document.getElementById('container').style.backgroundColor = color;
+	document.getElementById('output').style.color = color;
+	document.getElementById('output').innerHTML = color;
+}
+
